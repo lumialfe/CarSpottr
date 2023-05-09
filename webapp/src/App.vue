d4ed6c49fcecf9c1284cbf4278394226e90a2800
LuisMi Alonso <60442261+lumialfe@users.noreply.github.com>
2023-05-09 12:47:55 +0200

    Added Footer

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import Header from "./components/Header.vue";
-import Main from "./components/Main.vue";</script>
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";</script>
 
 <template>
   <Header></Header>
   <Main></Main>
+  <Footer></Footer>
 </template>
 
 <style lang="scss" scoped>
---
+++ webapp/src/components/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+  <footer>
+    <div class="footer--part">
+      <a href="https://github.com/lumialfe/CarSpottr"><i class="fab fa-github"></i>
+        GitHub Repository</a>
+      <a href="https://keras.io/examples/vision/deeplabv3_plus/"><i class="fa-sharp fa-solid fa-k"></i>
+        DeepLabV3+</a>
+      <a href="https://www.uniovi.es/"><i class="fa-solid fa-building-columns"></i>
+        UniOvi</a>
+    </div>
+    <div class="footer--part">
+      <a href="/about.html"><i class="fa-solid fa-user"></i>
+        About the author</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/style.scss";
+
+footer {
+  background-color: $primary-color;
+  font-family: 'Montserrat', sans-serif;
+
+  display: grid;
+  grid-template-columns: 50% 50%;
+
+  height: 100px;
+  padding: 50px 30%;
+
+  .footer--part {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 15px;
+
+    a {
+      color: $background-color;
+
+      &:hover {
+        cursor: pointer;
+        transition: all 0.2s ease-in-out;
+        font-weight: bold;
+      }
+    }
+  }
+}
+</style>
