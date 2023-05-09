ce55dce40efe3a1c5cc6e8c16a7507b4130a2606
Luis Miguel Alonso <UO270139@uniovi.es>
2023-05-09 17:01:00 +0200

    More Rest API

--- restapi/api.py
+++ restapi/api.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import get_file 
+from tensorflow.keras.utils import load_img 
+from tensorflow.keras.utils import img_to_array
 from fastapi.middleware.cors import CORSMiddleware
+from tensorflow import expand_dims
+from tensorflow.nn import softmax
+from numpy import argmax
+from numpy import max
+from numpy import array
+from json import dumps
 from uvicorn import run
 import os
 
 app = FastAPI()
+model_dir = "\\model\\model.h5"
+model = load_model(model_dir)
+
 
 origins = ["*"]
 methods = ["*"]
@@ -20,6 +33,33 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Welcome to the CarSpottr API!"}
+
+@app.post("/net/image/prediction/")
+async def get_net_image_prediction(image_link: str = ""):
+    if image_link == "":
+        return {"message": "No image link provided"}
+    
+    img_path = get_file(
+        origin = image_link
+    )
+    img = load_img(
+        img_path, 
+        target_size = (256, 256)
+    )
+
+    img_array = img_to_array(img)
+    img_array = expand_dims(img_array, 0)
+
+    pred = model.predict(img_array)
+    score = softmax(pred[0])
+
+    # class_prediction = class_predictions[argmax(score)]
+    # model_score = round(max(score) * 100, 2)
+
+    return {
+        "model-prediction": class_prediction,
+        "model-prediction-confidence-score": model_score
+    }
     
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
