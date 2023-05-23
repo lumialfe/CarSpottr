46a5eaeb72d545afaa76280e0603ca60f66f8760
LuisMi Alonso <lumialfe@hotmail.com>
2023-05-23 17:08:27 +0200

    Updated endpoint.js

--- webapp/src/store/endpoint.js
+++ webapp/src/store/endpoint.js
@@ -1,2 +1,2 @@
-//export const api_predict = 'http://localhost:5000/predict/';
-export const api_predict = 'http://localhost:8000/predict/';
+export const api_predict = 'http://localhost:5000/predict/';
+//export const api_predict = 'http://localhost:8000/predict/';
